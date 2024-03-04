4f76fc66871cd1eaf2967ab611c92553945edaa5
Aleksey Talapin <talexei@yandex.ru>
2024-03-04 20:05:53 +0300

    add test

--- src/test/java/test/numbergenerator/NumberGeneratorApplicationTests.java
+++ src/test/java/test/numbergenerator/NumberGeneratorApplicationTests.java
@@ -1,13 +1,26 @@
 package test.numbergenerator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import test.numbergenerator.entity.CarNumber;
+import test.numbergenerator.entity.CarNumberRepository;
 
 @SpringBootTest
 class NumberGeneratorApplicationTests {
-
+    @Autowired
+    private CarNumberRepository carNumberRepository;
+    private String carNumberRegex = "[А,Е,Т,О,Р,Н,У,К,Х,С,В,М]\\d{3}[А,Е,Т,О,Р,Н,У,К,Х,С,В,М]{2} 116 RUS";
     @Test
-    void contextLoads() {
+    void verifyCarNumberFormatRandom() {
+        String carNumber = carNumberRepository.getRandom().toString();
+        Assert.isTrue(carNumber.matches(carNumberRegex), "Номер не прошел проверку на шаблон");
+    }
+    @Test
+    void verifyCarNumberFormatNext() {
+        String carNumber = carNumberRepository.getNext().toString();
+        Assert.isTrue(carNumber.matches(carNumberRegex), "Номер не прошел проверку на шаблон");
     }
 
 }
